a7bb7ce834bdb1309515a83ce10a8374e8269344
kamil99998 <kamiilsieeradzki@gmail.com>
2025-04-22 20:12:10 +0200

    na nowo

--- PresentationModel/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: InternalsVisibleTo("PresentationModelTest")]
-[assembly: ComVisible(false)]
-[assembly: Guid("8424f3e6-6484-4029-8743-bb762c82077a")]
